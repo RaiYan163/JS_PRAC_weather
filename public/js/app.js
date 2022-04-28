6c1b0c92751d5887b13564f41ae39618a1cacc18
RaiYan_163 <raiyanashraf20@gmail.com>
2022-04-29 02:02:48 +0600

    heroku launch

--- public/js/app.js
+++ public/js/app.js
@@ -18,20 +18,18 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = "loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          // console.log(data.error);
-          messageOne.textContent = data.error;
-        } else {
-          // console.log(data.location);
-          // console.log(data.forecast);
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        // console.log(data.location);
+        // console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
   //console.log(location);
 });
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const hbs = require("hbs");
 const { query } = require("express");
 
@@ -138,6 +140,6 @@ app.get("*", (req, res) => {
 //app.com/help
 //app.com/about
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
