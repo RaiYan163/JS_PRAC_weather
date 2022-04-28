b77144ba45430f87ac61cbcdf0169421b77377ec
RaiYan_163 <raiyanashraf20@gmail.com>
2022-04-28 14:36:56 +0600

    Remove unncessary console.log output

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is loaded");
-
 // fetch("http://puzzle.mead.io/puzzle").then((response) => {
 //   response.json().then((data) => {
 //     console.log(data);
